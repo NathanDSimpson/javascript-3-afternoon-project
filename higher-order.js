ae5a1b30dfe5070a4f41d2f929fe20b1fdfc0157
Nathan Simpson <ndsimpson01@gmail.com>
2019-03-22 15:42:28 -0600

    Completed higher-order

--- higher-order.js
+++ higher-order.js
@@ -21,7 +21,9 @@ const mixedNumbers = [6,3,1,7,5,2,6,8,9,4,2,7,9,3,1,8,4,3];
 */
 
 //Code Here
-let evenNumbers // = mixedNumbers.filter(/* Provide Your Callback Here */)
+let evenNumbers  = mixedNumbers.filter(function(num){
+  return num % 2 === 0
+})
 
 
 
@@ -44,10 +46,9 @@ const prices = [15.00, 23.00, 78.00, 34.00, 12.00, 86.00, 12.00, 79.00, 32.00];
 */
 
 //Code Here
-let postTaxPrices // = prices.map(/* Provide Your Callback Here );
-
-
-
+let postTaxPrices = prices.map(function(num){
+  return num * 1.07
+})
 ////////// PROBLEM 3 //////////
 
 // Do not edit the code below.
@@ -63,7 +64,9 @@ const populations = [8175133, 3792621, 2695598, 2100263];
 */
 
 //Code Here
-let totalPopulation //  = populations.reduce(/* Provide Your Callback Here */)
+let totalPopulation = populations.reduce(function(accum, num){
+  return accum + num
+})
 
 
 
@@ -89,7 +92,9 @@ const monstersInYourPocket = [{"monster":"Bulbabunny","CP":156},{"monster":"Bulb
 */
 
 //Code Here
-let myStrongest // = monstersInYourPocket.filter(/* Provide Your Callback Here */)
+let myStrongest = monstersInYourPocket.filter(function(monster){
+  return monster['CP'] > 200
+  })
 
 
 
@@ -106,7 +111,9 @@ const orders = [{"price":15,"tax":0.09},{"price":42,"tax":0.07},{"price":56,"tax
   Use a higher order method to get all the order totals after adding in the sales tax. Your answer should be an array of numbers, one total for each order.
 */
 
-let orderTotals // Code here
+let orderTotals = orders.map(function(order){
+  return order.price * (1 + order.tax)
+})
 
 
 
@@ -126,6 +133,16 @@ const purchases = [{"owner":"Barry","price":103},{"owner":"Bob","price":75},
   Use a high order method to create to get the sum of bobsTotal.
 */
 
-let bobsTotal //Code Here
+let bobsPurchases = purchases.filter(function(transaction){
+  return transaction.owner === "Bob"
+})
+
+let bobArr = bobsPurchases.map(function(trans){
+  return trans.price
+})
+
+let bobsTotal = bobArr.reduce(function(accum, current){
+  return accum + current
+})
 
 
